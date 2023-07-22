fca6c7133442d834bf2a0fff65da2fc0a73e210a
tvssp1997 <turlsaiprakash@gmail.com>
2023-07-22 20:00:05 +0000

    second version

--- src/components/MoneyDetails/index.js
+++ src/components/MoneyDetails/index.js
@@ -1,1 +1,51 @@
-// Write your code here
+import './index.css'
+
+const MoneyDetails = props => {
+  const {yourBalance} = props
+
+  return (
+    <div className="bg-container">
+      <div className="balance-container">
+        <div className="image-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png "
+            alt="balance"
+            className="image-element"
+          />
+        </div>
+        <div className="content-container">
+          <h1 className="content-heading">Your Balance</h1>
+          <p className="content-heading">Rs 0</p>
+        </div>
+      </div>
+      <div className="income-container">
+        <div className="image-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png"
+            alt="income"
+            className="image-element"
+          />
+        </div>
+        <div className="content-container">
+          <h1 className="content-heading">Your Balance</h1>
+          <p className="content-heading">Rs 0</p>
+        </div>
+      </div>
+      <div className="expense-container">
+        <div className="image-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png "
+            alt="expenses"
+            className="image-element"
+          />
+        </div>
+        <div className="content-container">
+          <h1 className="content-heading">Your Balance</h1>
+          <p className="content-heading">Rs 0</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MoneyDetails
--- src/components/MoneyManager/index.js
+++ src/components/MoneyManager/index.js
@@ -1,12 +1,36 @@
-const transactionTypeOptions = [
-  {
-    optionId: 'INCOME',
-    displayText: 'Income',
-  },
-  {
-    optionId: 'EXPENSES',
-    displayText: 'Expenses',
-  },
-]
+// const transactionTypeOptions = [
+//   {
+//     optionId: 'INCOME',
+//     displayText: 'Income',
+//   },
+//   {
+//     optionId: 'EXPENSES',
+//     displayText: 'Expenses',
+//   },
+// ]
 
-// Write your code here
+import {Component} from 'react'
+import './index.css'
+import MoneyDetails from '../MoneyDetails'
+
+class MoneyManager extends Component {
+  state = {}
+
+  render() {
+    return (
+      <div className="main-container">
+        <div className="first-container">
+          <h1 className="fc-heading">Hi, Richard</h1>
+          <p className="fc-para">
+            Welcome back to your <span className="fc-span">Money Manager</span>
+          </p>
+        </div>
+        <div>
+          <MoneyDetails />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MoneyManager
